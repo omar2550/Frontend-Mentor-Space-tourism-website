8305da06fc7ce3997aeb74c91e6e96f933660ac0
omar2550 <omr222000@gmail.com>
2025-06-17 19:31:17 +0300

    Update tech.js

--- js/tech.js
+++ js/tech.js
@@ -1,9 +1,9 @@
 window.onload = function () {
   if (window.innerWidth >= 992) {
-    document.body.style.backgroundImage = `url("/assets/technology/background-technology-desktop.jpg")`;
+    document.body.style.backgroundImage = `url("assets/technology/background-technology-desktop.jpg")`;
   } else if (window.innerWidth >= 768) {
-    document.body.style.backgroundImage = `url("/assets/technology/background-technology-tablet.jpg")`;
+    document.body.style.backgroundImage = `url("assets/technology/background-technology-tablet.jpg")`;
   } else {
-    document.body.style.backgroundImage = `url("/assets/technology/background-technology-mobile.jpg")`;
+    document.body.style.backgroundImage = `url("assets/technology/background-technology-mobile.jpg")`;
   }
 };
