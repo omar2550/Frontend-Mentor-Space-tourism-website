4fd5fcebc789fdbac6d4e5948a390d54008d126d
omar2550 <omr222000@gmail.com>
2025-06-17 19:25:19 +0300

    Update crew.js

--- js/crew.js
+++ js/crew.js
@@ -1,9 +1,9 @@
 window.onload = function () {
   if (window.innerWidth >= 992) {
-    document.body.style.backgroundImage = `url("../../assets/crew/background-crew-desktop.jpg")`;
+    document.body.style.backgroundImage = `url("/assets/crew/background-crew-desktop.jpg")`;
   } else if (window.innerWidth >= 768) {
-    document.body.style.backgroundImage = `url("../../assets/crew/background-crew-tablet.jpg")`;
+    document.body.style.backgroundImage = `url("/assets/crew/background-crew-tablet.jpg")`;
   } else {
-    document.body.style.backgroundImage = `url("../../assets/crew/background-crew-mobile.jpg")`;
+    document.body.style.backgroundImage = `url("/assets/crew/background-crew-mobile.jpg")`;
   }
 };
